136a4c15ddb79ad5a0631d2287adc627ea36c223
Quentin <quentin@MacBook-Pro-de-Quentin-2.local>
2025-12-07 01:24:38 +0100

    add

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 export { db };
