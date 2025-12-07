4b36966b82d3369ee3690d889e0dc8ee8c1f9a16
Quentin <quentin@MacBook-Pro-de-Quentin-2.local>
2025-12-07 22:17:11 +0100

    add config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,17 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_MEASUREMENT_ID,
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID,
 };
 
-console.log('API KEY USED BY FIREBASE 👉', firebaseConfig.apiKey);
-
+// DEBUG visible aussi en prod
+if (typeof window !== 'undefined') {
+  window.__FIREBASE_CONFIG__ = firebaseConfig;
+}
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
